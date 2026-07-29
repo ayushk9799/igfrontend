d79d8dbfc33d20f5057b262c99785087ceb03335
ayushk9799 <ayushkumarsanu00@gmail.com>
2026-07-29 19:58:47 +0530

    chnages

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,5 +1,5 @@
 // API Configuration
 // Update this URL for production
 //  export const API_BASE = 'https://citizen-basename-sized-providence.trycloudflare.com';
-export const API_BASE = 'http://10.199.207.231:3000';
+export const API_BASE = 'https://thethousandways.com';
 export const API_URL = API_BASE; // Alias for backwards compatibility
